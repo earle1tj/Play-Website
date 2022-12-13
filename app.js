69a08a3de36878872eaff645a3682d6c04ffabc7
Taylon Earle <earle.taylon@gmail.com>
2022-12-13 18:22:34 -0500

    initial publish

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const nodemailer = require('nodemailer');
+require('dotenv').config()
+const http = require('https')
